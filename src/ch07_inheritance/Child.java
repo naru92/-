310b942dcf239954b9f07430eb9a13aa4003dac1
naru92 <skfn8000@naver.com>
2021-09-25 02:55:45 +0900

    inherit example

---
+++ src/ch07_inheritance/Child.java
@@ -0,0 +1,5 @@
+package ch07_inheritance;
+
+public class Child extends Parent{
+
+}
---
+++ src/ch07_inheritance/Parent.java
@@ -0,0 +1,5 @@
+package ch07_inheritance;
+
+public class Parent {
+	private static String firstName = "Kang";
+}
